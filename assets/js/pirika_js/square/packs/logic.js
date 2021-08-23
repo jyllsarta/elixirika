d16680178e1b8bbae94ee5ed796efd1b09bb4829
jyllsarta <illstarred.dive@gmail.com>
2021-08-23 15:56:25 +0900

    ロジックとの結合

---
+++ assets/js/pirika_js/square/packs/logic.js
@@ -0,0 +1,5 @@
+module.exports = class Logic {
+  constructor() {
+    this.deck = [3, 4, 5];
+  }
+};
--- assets/js/pirika_js/square/square.vue
+++ assets/js/pirika_js/square/square.vue
@@ -2,17 +2,24 @@
   #app
     h1
       | すくえあ！
+    p
+      | {{ logic.deck }}
 
 </template>
 
 <script lang="typescript">
-    import Vue from 'vue'
+    import Vue from 'vue';
+    import Logic from "./packs/logic";
 
     export default Vue.extend({
     components: {
     },
+    mounted(){
+      this.logic = new Logic();
+    },
     data(){
       return {
+        logic: {},
       };
     },
   })
