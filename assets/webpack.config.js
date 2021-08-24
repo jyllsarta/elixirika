682fcf1dcbfdcd10481c53983029607f3de300ea
jyllsarta <illstarred.dive@gmail.com>
2021-08-24 22:13:04 +0900

    webpackのアップデート

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const glob = require('glob');
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
@@ -113,6 +112,5 @@ module.exports = (env, options) => {
       new MiniCssExtractPlugin({ filename: '../css/[name].css' }),
       new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
     ]
-    .concat(devMode ? [new HardSourceWebpackPlugin()] : [])
   }
 };
