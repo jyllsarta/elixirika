f5d67a9d0db79869a56d060db96f0e24319eaf0c
jyllsarta <illstarred.dive@gmail.com>
2021-09-06 23:41:47 +0900

    起動ポートを選択できるようにする

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :elixirika, Elixirika.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :elixirika, ElixirikaWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: System.get_env("PORT") || 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
