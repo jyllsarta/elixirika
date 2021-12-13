fc70299a73616a0e87e2f3b0d85e7a762a5b810b
jyll sarta <illstarred.dive@gmail.com>
2021-12-14 00:38:31 +0900

    メッセージ欄ちょっと整えましょう

--- assets/js/pirika_js/square/SupportCharacterMessage.vue
+++ assets/js/pirika_js/square/SupportCharacterMessage.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
   .area
-    .balloon2.with_shadow
+    img.background.with_drop_shadow(src="images/square/svg/message.svg")
+    .text
       | {{message}}
 </template>
 
@@ -24,37 +25,19 @@
   .area{
     width: 250px;
     height: 100px;
-    .balloon2 {
-      position: relative;
-      display: inline-block;
+    position: relative;
+    .background{
+      position: absolute;
+      width: 100%;
+    }
+    .text {
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      line-height: 140%;
       white-space: pre-wrap;
-      line-height: 100%;
       padding: $space-m;
-      width: 250px;
       color: $white;
-      background: $ingame-background;
-      border: 1px solid $white;
-    }
-
-    .balloon2:before {
-      content: "";
-      position: absolute;
-      bottom: -27px;
-      left: 20%;
-      margin-left: -17px;
-      border: 14px solid transparent;
-      border-top: 14px solid $ingame-background;
-      z-index: 2;
-    }
-    .balloon2:after {
-      content: "";
-      position: absolute;
-      bottom: -28px;
-      left: 20%;
-      margin-left: -17px;
-      border: 14px solid transparent;
-      border-top: 14px solid $white;
-      z-index: 1;
     }
   }
 </style>
