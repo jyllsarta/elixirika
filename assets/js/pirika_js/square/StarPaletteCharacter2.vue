e2b504de8203418c324557eeed5882d72e8b4a29
jyll sarta <illstarred.dive@gmail.com>
2021-12-28 18:29:58 +0900

    delta表示できるようにしようぜ！

--- assets/js/pirika_js/square/StarPaletteCharacter2.vue
+++ assets/js/pirika_js/square/StarPaletteCharacter2.vue
@@ -6,13 +6,16 @@
       .gauge
         .foreground
           .fill(:style="{width: `${currentFillWidth * 100}%`}", :class="currentFillStatus")
-          .lose_next(:style="{width: `${currentLoseWidth * 100}%`}")
+          .lose_next(:style="{width: `${expectedEnergyDeltaWidth * 100}%`}" v-if="expectedEnergyDelta < 0")
+          .gain_next(:style="{width: `${expectedEnergyDeltaWidth * 100}%`}" v-if="expectedEnergyDelta > 0")
     .number
       .energy
         .current
           | {{energy}}
         .max
           | / {{maxEnergy}}
+    .delta_number(v-if="holdingCard" :class="holdingCardClass")
+      | {{expectedEnergyDelta > 0 ? '+' : ''}}{{expectedEnergyDelta}}
 </template>
 
 <script lang="typescript">
@@ -62,6 +65,34 @@
           return "high"
         }
         return "best"
+      },
+      holdingCard(){
+        return this.model.getHoldingCard();
+      },
+      expectedFieldScore(){
+        const index = this.model.selectingBoardIndex;
+        const card = this.holdingCard;
+        const board = this.model.board.fields[index];
+        const holdingCardIsSender = card && card.isSenderCard();
+        if(board && card && holdingCardIsSender && this.model.cardStackRule(this.model.character, this.model, card, board)){
+          return this.model.board.fields[index].scoreWithCard(card);
+        }
+        return 0;
+      },
+      expectedEnergyDelta(){
+        return -this.consumptionPerCard + this.expectedFieldScore;
+      },
+      expectedEnergyDeltaWidth(){
+        return Math.abs(this.expectedEnergyDelta) / this.maxEnergy;
+      },
+      holdingCardClass(){
+        if(this.expectedEnergyDelta > 0){
+          return "plus";
+        }
+        if(this.expectedEnergyDelta < 0){
+          return "minus";
+        }
+        return "even";
       }
     },
   })
@@ -116,6 +147,11 @@
             background-color: $yellow2;
             animation: flash 4s linear infinite;
           }
+          .gain_next{
+            height: 100%;
+            background-color: $red2;
+            animation: flash 4s linear infinite;
+          }
         }
       }
     }
@@ -141,6 +177,24 @@
           font-size: $font-size-large;
           width: 3rem;
         }
+      }
+    }
+    .delta_number{
+      position: absolute;
+      display: flex;
+      left: 90px;
+      top: 20px;
+      line-height: 100%;
+      width: 3rem;
+      text-align: right;
+      &.plus{
+        color: $red1;
+      }
+      &.minus{
+        color: $blue1;
+      }
+      &.even{
+        color: $white;
       }
     }
 
