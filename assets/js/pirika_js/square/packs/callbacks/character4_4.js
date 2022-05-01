98b7621bfc66b5291bdb5e9a2207e6dc59a953d9
jyll sarta <illstarred.dive@gmail.com>
2022-05-01 17:42:00 +0900

    難易度調整

--- assets/js/pirika_js/square/packs/callbacks/character4_4.js
+++ assets/js/pirika_js/square/packs/callbacks/character4_4.js
@@ -2,8 +2,8 @@ module.exports = class Character4_4 {
   starPaletteParameter(){
     return {
       enemies: [
-        {id: 1, order: 1, hp: 15, image: "spica", maxDamage: 3},
-        {id: 2, order: 2, hp: 15, image: "tirol", shield: 10},
+        {id: 1, order: 1, hp: 20, image: "spica", maxDamage: 3},
+        {id: 2, order: 2, hp: 20, image: "tirol", shield: 10},
       ]
     };
   }
