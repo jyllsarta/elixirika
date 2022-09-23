27f84a591f81966b96c3e84944d3fe04cab20944
jyll sarta <illstarred.dive@gmail.com>
2022-09-24 01:21:06 +0900

    色々スニペット残す

--- assets/js/pirika_js/rhomb/CanvasTest.vue
+++ assets/js/pirika_js/rhomb/CanvasTest.vue
@@ -3,19 +3,51 @@
   .field
     .title
       | CANVAS TEST
-    .canvas
+    canvas.canvas(ref="canvas", width="640" height="480")
 </template>
 
 <script lang="typescript">
   import Vue from 'vue';
 
   export default Vue.extend({
-    created(){
+    mounted(){
       this.onLoad();
     },
     methods: {
       onLoad(){
-        console.log("canvas load!");
+        this.draw();
+      },
+      draw(){
+        const canvas = this.$refs.canvas;
+        const context = canvas?.getContext("2d");
+        context.translate(.5,.5);
+
+        context.strokeStyle='cyan';
+        context.strokeRect(100, 50, 120, 60);
+
+        context.beginPath();
+        context.setLineDash([10,5]);
+        context.moveTo(100, 100);
+        context.lineTo(250, 200); 
+        context.lineTo(350, 200); 
+        context.lineTo(250, 100); 
+        context.stroke();
+
+        context.shadowColor='white';
+        context.shadowOffsetX = 4;
+        context.shadowOffsetY = 4;        
+
+        context.beginPath();
+        context.moveTo(200, 200);
+        context.lineTo(400, 400);
+        context.lineTo(200, 400);
+        context.lineTo(400, 200);
+        const grad = context.createLinearGradient(0,0,500,0);
+        grad.addColorStop(0, 'cyan');
+        grad.addColorStop(1, 'red');
+        context.strokeStyle = grad;
+        context.lineWidth = 1;
+        context.stroke();
       }
     }
   })
@@ -32,6 +64,9 @@
       color: cyan;
     }
     .canvas{
+      position: relative;
+      width: 640px;
+      height: 480px;
     }
   }
 </style>
