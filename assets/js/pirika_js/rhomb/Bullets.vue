2529a40a3864fd7a30eff5ecdf0eb1f68f713bab
jyll sarta <illstarred.dive@gmail.com>
2022-10-28 02:00:10 +0900

    ちょっとずつずれる現象解明

--- assets/js/pirika_js/rhomb/Bullets.vue
+++ assets/js/pirika_js/rhomb/Bullets.vue
@@ -39,6 +39,7 @@
       drawLines(){
         const canvas = this.$refs.canvas;
         const context = canvas?.getContext("2d");
+        context.setTransform(1,0,0,1,0,0);
         context.clearRect(0, 0, 1200, 800);
         context.translate(.5,.5);
         context.setLineDash([30, 15]);
