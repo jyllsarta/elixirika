9eee4d888ceb7ce8eaf36c833926b6e17d134029
jyll sarta <illstarred.dive@gmail.com>
2022-10-30 22:33:21 +0900

    面白さ重視のプロトタイプボス

--- assets/js/pirika_js/rhomb/masterdata/turns.js
+++ assets/js/pirika_js/rhomb/masterdata/turns.js
@@ -1,6 +1,7 @@
 module.exports = [
   // 将来的にenemy_id やら ループ表現を取り込む必要ありそう
-  {id: 1, turn: 1, bullet_ids: [1, 2]},
-  {id: 2, turn: 2, bullet_ids: [3, 4]},
-  {id: 3, turn: 3, bullet_ids: [5]},
+  {id: 1, turn: 1, bullet_ids: [11, 12, 13, 14, 15, 16, 17, 18, 19]},
+  {id: 2, turn: 2, bullet_ids: [21, 22, 23, 24, 25]},
+  {id: 3, turn: 3, bullet_ids: [31, 32]},
+  {id: 4, turn: 4, bullet_ids: [41, 42, 43, 44, 45]},
 ]
