09dd06a44e83369592e1877f4865df4c9468d9f2
jyll sarta <illstarred.dive@gmail.com>
2023-04-24 22:19:16 +0900

    敵の表情

--- assets/js/pirika_js/queens/EnemyImage.vue
+++ assets/js/pirika_js/queens/EnemyImage.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="enemy_image" v-if="state">
-  <img src="/images/queens/characters/queens1.png" alt="" class="enemy">
+<img :src="`/images/queens/characters/queens${faceId}.png`" alt="" class="enemy">
 </div>
 </template>
 
@@ -10,6 +10,22 @@ export default {
   props: {
     state: State
   },
+  computed: {
+    faceId(){
+      const target = this.state.board.targetValue();
+      const distance = target - this.state.enemyBoard.targetValue(target);
+      if(distance === 0){
+        return 1;
+      }
+      if(distance < 0){
+        return 4;
+      } 
+      if(distance <= 2){
+        return 2;
+      }
+      return 3;
+    }
+  }
 }
 </script>
 
