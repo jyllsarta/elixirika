e4220b81b9f4f40c4ab2039f439a749341ebc51a
jyll sarta <illstarred.dive@gmail.com>
2023-05-13 23:27:52 +0900

    シンプルなシーン管理システム

--- assets/js/pirika_js/queens/GameWindow.vue
+++ assets/js/pirika_js/queens/GameWindow.vue
@@ -1,163 +1,35 @@
 <template>
-  <div class="game_window">
-    <enemy-image class="enemy_image component" :state="state" />
-    <enemy-hand class="enemy_hand component" :state="state" />
-    <enemy-message class="enemy_message component" :state="state" />
-    <bankroll class="bankroll component" :state="state" />
-    <game-table class="game_table component" :state="state" :controller="controller" />
-    <turn-end-button class="turn_end_button component" :state="state" :controller="controller" />
-    <player-hand class="player_hand component" :state="state" :controller="controller"/>
-    <side-pane class="side_pane component" :state="state" :controller="controller"/>
-    <option-menu class="menu component" @click="showTutorial = true"/>
-    <tutorial-dialog class="dialog component" v-if="showTutorial" @close="showTutorial = false"/>
-    <first-break-dialog class="dialog component" v-if="showFirstBreak" @close="nextPhase"/>
-    <second-break-dialog class="dialog component" v-if="showSecondBreak" @close="nextPhase"/>
-    <gameset-dialog class="dialog component" v-if="state?.phase == 'game_end'" @close="nextPhase" :state="state" />
-    <phase-mover :state="state" :controller="controller" />
+  <div class="game_frame">
+    <in-game-scene v-if="$store.state.scene.name == 'in_game'"/>
   </div>
 </template>
 
 <script>
-import State from "./packs/model/state";
-import Controller from "./packs/service/controller";
-import EnemyHand from "./EnemyHand.vue";
-import EnemyImage from "./EnemyImage.vue";
-import EnemyMessage from "./EnemyMessage.vue";
-import PlayerHand from "./PlayerHand.vue";
-import GameTable from "./GameTable.vue";
-import SidePane from './SidePane.vue';
-import PhaseMover from './PhaseMover.vue';
-import OptionMenu from './OptionMenu.vue';
-import Bankroll from './Bankroll.vue';
-import TutorialDialog from './TutorialDialog.vue';
-import FirstBreakDialog from './FirstBreakDialog.vue';
-import SecondBreakDialog from './SecondBreakDialog.vue';
-import GamesetDialog from './GamesetDialog.vue';
-import TurnEndButton from './TurnEndButton.vue';
+import InGameScene from './InGameScene.vue';
+import store from "./packs/store";
 
 export default {
+  store,
   components: {
-    PlayerHand,
-    EnemyHand,
-    EnemyImage,
-    EnemyMessage,
-    GameTable,
-    SidePane,
-    PhaseMover,
-    OptionMenu,
-    Bankroll,
-    TutorialDialog,
-    FirstBreakDialog,
-    SecondBreakDialog,
-    GamesetDialog,
-    TurnEndButton
-  },
-  data(){
-    return {
-      state: null,
-      controller: null,
-      showTutorial: false,
-    }
+    InGameScene,
   },
   mounted(){
-    this.state = new State();
-    window.state = this.state;
-    this.controller = new Controller(state);
-    window.controller = this.controller;
+    this.$store.commit("loadScene", {name: "in_game"});
   },
   methods: {
-    nextPhase(){
-      this.controller.nextPhase();
-    }
   },
   computed: {
-    showFirstBreak(){
-      if(!this.state){
-        return false;
-      }
-      return this.state.phase == "unstarted" && this.state.persistentData.winCount == 1;
-    },
-    showSecondBreak(){
-      if(!this.state){
-        return false;
-      }
-      return this.state.phase == "unstarted" && this.state.persistentData.winCount == 2;
-    },
   }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "stylesheets/global_settings";
-.game_window{
+.game_frame{
   width: $window-width;
   height: $window-height;
   position: relative;
   background-color: $bg4;
   color: $white;
-
-  .component{
-    position: absolute;
-  }
-  .menu{
-    top: 10px;
-    left: 10px;
-    width: 50px;
-    height: 50px;
-  }
-  .enemy_image{
-    top: 0px;
-    left: 30px;
-    width: 400px;
-    height: 200px;
-  }
-  .enemy_hand{
-    top: 170px;
-    left: 10px;
-    width: 500px;
-    height: 80px;
-  }
-  .enemy_message{
-    top: 60px;
-    left: 300px;
-    width: 250px;
-    height: 80px;
-  }
-  .bankroll{
-    top: 10px;
-    right: 320px;
-    width: 200px;
-    height: 100px;
-  }
-  .turn_end_button{
-    top: 200px;
-    right: 320px;
-    width: 200px;
-    height: 100px;
-  }
-  .game_table{
-    top: 260px;
-    left: 10px;
-    width: 870px;
-    height: 220px;
-  }
-  .player_hand{
-    bottom: 10px;
-    left: 10px;
-    width: 870px;
-    height: 100px;
-  }
-  .side_pane{
-    top: 10px;
-    right: 10px;
-    width: 300px;
-    height: 540px;
-  }
-  .dialog{
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
 }
 </style>
--- assets/js/pirika_js/queens/packs/store.js
+++ assets/js/pirika_js/queens/packs/store.js
@@ -1,8 +1,17 @@
 import { createStore } from "vuex";
 
 export const store = createStore({
-  state: {},
+  state: {
+    scene: {
+      name: "title",
+      params: {},
+    },
+  },
   getters: {},
-  mutations: {},
+  mutations: {
+    loadScene(state, params) {
+      state.scene = params;
+    },
+  },
 });
 export default store;
