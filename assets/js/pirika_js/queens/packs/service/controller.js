440e6c6c7bffc1dfabb4bbcdcbed4f2ed70c23e5
jyll sarta <illstarred.dive@gmail.com>
2023-06-24 00:39:47 +0900

    スキル処理機構の具体化

--- assets/js/pirika_js/queens/packs/service/controller.js
+++ assets/js/pirika_js/queens/packs/service/controller.js
@@ -53,43 +53,6 @@ export class Controller {
     }
   }
 
-  _doUseSkillPlayer(cardId){
-    if(this.state.player.specialPoint < 2){
-      console.warn("insufficient sp");
-      this.state.uiState.selectSkillTarget = false;
-      return;
-    }
-    const card = this.state.player.hand.pickByCardId(cardId);
-    if(card){
-      this.state.discard.add(card);
-      this.state.player.hand.add(this.state.deck.draw());
-      this.state.player.hand.cards.forEach(card => card.reveal());
-      this.state.player.specialPoint -= 2;
-    }
-    this.state.uiState.selectSkillTarget = false;
-  }
-
-  useRejectDuplicateSkillPlayer(){
-    if(this.state.player.specialPoint < 2){
-      console.warn("insufficient sp");
-      return;
-    }
-    this.state.player.board.sendAllCardsTo(state.player.hand);
-    const duplications = this.state.player.hand.cards.filter(card => this.state.player.hand.cards.some(anotherCard => anotherCard.n == card.n && card.id !== anotherCard.id));
-    if(duplications.length === 0){
-      console.warn("no duplications");
-      return;
-    }
-    for(let card of duplications){
-      this.state.discard.add(this.state.player.hand.pickByCardId(card.id));
-    }
-    for(let i = 0; i < duplications.length; i++){
-      this.state.player.hand.add(state.deck.draw());
-      this.state.player.hand.cards.forEach(card => card.reveal());
-    }
-    this.state.player.specialPoint -= 2;
-  }
-
   unstage(cardId){
     const card = this.state.player.board.pickByCardId(cardId);
     if(card){
--- assets/js/pirika_js/queens/packs/service/skill_facade.js
+++ assets/js/pirika_js/queens/packs/service/skill_facade.js
@@ -4,10 +4,70 @@ export class SkillFacade {
   invoke(state, member, skillId, maybeCardId){
     const skill = Masterdata.get("skills", skillId);
     console.log(skill);
-    // フェーズバリデーション
-    // コスト計算
-    // エフェクト処理
-    // コスト支払い
+    if(state.phase !== "player_select"){
+      console.warn("invalid phase");
+      return;
+    }
+    if(member.specialPoint < skill.cost){
+      console.warn("insufficient sp");
+      return;
+    }
+    const result1 = this.resolveSkillEffect(state, member, skill, maybeCardId, skill.effect_key1, skill.effect_value1);
+    const result2 = this.resolveSkillEffect(state, member, skill, maybeCardId, skill.effect_key2, skill.effect_value2);
+    if(result1 || result2){
+      member.specialPoint -= skill.cost;
+    }
+  }
+
+  resolveSkillEffect(state, member, skill, maybeCardId, effectKey, effectValue){
+    if(!effectKey){
+      return false;
+    }
+    if(this["validate_" + effectKey] && !this["validate_" + effectKey](state, member, skill, maybeCardId, effectValue)){
+      return false;
+    }
+    this[effectKey](state, member, skill, maybeCardId, effectValue);
+    return true;
+  }
+
+  // 以下 skill effect
+
+  draw(state, member, skill, maybeCardId, effectValue){
+    for(let i = 0; i < effectValue; i++){
+      const card = state.deck.draw();
+      card.reveal();
+      if(card){
+        member.hand.add(card);
+      }
+    }
+  }
+
+  discardTarget(state, member, skill, maybeCardId, effectValue){
+    const card = member.hand.pickByCardId(maybeCardId);
+    if(card){
+      state.discard.add(card);
+    }
+  }
+
+  validate_sweep(state, member, skill, maybeCardId, effectValue){
+    state.player.board.sendAllCardsTo(state.player.hand);
+    const duplications = state.player.hand.cards.filter(card => state.player.hand.cards.some(anotherCard => anotherCard.n == card.n && card.id !== anotherCard.id));
+    if(duplications.length === 0){
+      console.warn("no duplications");
+      return false;
+    }
+    return true;
+  }
+
+  sweep(state, member, skill, maybeCardId, effectValue){
+    const duplications = state.player.hand.cards.filter(card => state.player.hand.cards.some(anotherCard => anotherCard.n == card.n && card.id !== anotherCard.id));
+    for(let card of duplications){
+      state.discard.add(state.player.hand.pickByCardId(card.id));
+    }
+    for(let i = 0; i < duplications.length; i++){
+      state.player.hand.add(state.deck.draw());
+      state.player.hand.cards.forEach(card => card.reveal());
+    }
   }
 };
 export default SkillFacade;
