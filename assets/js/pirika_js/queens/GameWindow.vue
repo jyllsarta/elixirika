87cc72677175775b6b99984afcdb3406dcb12039
jyll sarta <illstarred.dive@gmail.com>
2023-07-23 16:42:13 +0900

    GamesetDialogのコンテンツ実装

--- assets/js/pirika_js/queens/GameWindow.vue
+++ assets/js/pirika_js/queens/GameWindow.vue
@@ -26,7 +26,7 @@ export default {
     EquipmentScene,
   },
   mounted(){
-    this.$store.commit("loadScene", {name: "in_game", questId: 201});
+    this.$store.commit("loadScene", {name: "in_game", questId: 101});
   },
   methods: {
   },
--- assets/js/pirika_js/queens/in_game/GamesetDialog.vue
+++ assets/js/pirika_js/queens/in_game/GamesetDialog.vue
@@ -2,6 +2,15 @@
   <div class="dialog" v-if="state">
     <div class="_back" @click="backToMenu"/>
     <div class="content">
+      <div class="title">
+        {{ state.isWin() ? "勝利！" : "まけ" }}
+      </div>
+      <div class="character">
+        <img :src="`images/queens/characters/character${quest?.character_id}_1.png`" class="image">
+      </div>
+      <div class="script">
+        {{ state.isWin() ? quest?.win_script : quest?.lose_script }}
+      </div>
       <div class="close">
         <div class="button" @click="backToMenu">
           メニューに戻る
@@ -15,6 +24,7 @@
 import State from "../packs/model/state";
 import Savedata from '../packs/savedata';
 import store from '../packs/store';
+import Masterdata from '../packs/masterdata';
 
 export default {
   store,
@@ -28,6 +38,11 @@ export default {
       this.$store.commit("loadScene", {name: "menu"});
     }
   },
+  computed: {
+    quest(){
+      return Masterdata.get("quests", this.state.questId);
+    }
+  }
 };
 </script>
 
@@ -36,8 +51,6 @@ export default {
 .dialog{
   ._back {
     position: absolute;
-    // 画面横幅のなにもない領域 - 20px まで当たり判定を持つ
-    right: calc((1200px - 100vw + 20px) / 2);
     top: 0;
     width: 100vw;
     height: 90vh;
@@ -51,10 +64,34 @@ export default {
     width: 60%;
     background-color: $bg3;
     border: 2px solid $base2;
+    .title{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 20%;
+      font-size: $font-size-large;
+    }
+    .character{
+      position: relative;
+      width: 100%;
+      height: 50%;
+      overflow: hidden;
+      .image{
+        transform: translateY(-10%);
+        width: 100%;
+      }
+    }
+    .script{
+      height: 10%;
+      padding: 8px;
+      width: 100%;
+      text-align: center;
+    }
     .close{
       display: flex;
       justify-content: center;
       align-items: center;
+      height: 20%;
       .button{
         border: 1px solid $base2;
         padding: 20px;
