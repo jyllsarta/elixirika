f3c8e077b87794074b43265f925dbfa8a8c3f7b6
jyll sarta <illstarred.dive@gmail.com>
2023-07-29 15:41:11 +0900

    買ったら暗くなる

--- assets/js/pirika_js/queens/ShopItem.vue
+++ assets/js/pirika_js/queens/ShopItem.vue
@@ -1,26 +1,36 @@
 <template>
-  <div class="item" :class="{disabled: disabled()}">
-    <div class="icon_area">
-      <shop-item-icon
-        :shop-item="shopItem"
-        :equipment="equipment"
-      />
+  <div class="wrapper">
+    <div class="item">
+      <div class="icon_area">
+        <shop-item-icon
+          :shop-item="shopItem"
+          :equipment="equipment"
+        />
+      </div>
+      <div class="main">
+        <div class="name">{{equipment.title}}</div>
+        <div class="description">{{ equipment.description }}</div>
+      </div>
+      <div class="price_area">
+        <div class="coin_icon tentative_panel"></div>
+        <div class="price">{{ shopItem.price }}</div>
+      </div>
+      <div class="buy_area">
+        <div 
+          class="buy_button tentative_button"
+          @click="buy"
+          :class="{disabled: !canBuy()}"
+        >
+          買う
+        </div>
+      </div>
     </div>
-    <div class="main">
-      <div class="name">{{equipment.title}}</div>
-      <div class="description">{{ equipment.description }}</div>
-    </div>
-    <div class="price_area">
-      <div class="coin_icon tentative_panel"></div>
-      <div class="price">{{ shopItem.price }}</div>
-    </div>
-    <div class="buy_area">
-      <div 
-        class="buy_button tentative_button"
-        @click="buy"
-        :class="{disabled: !canBuy()}"
-      >
-        買う
+    <div class="lock_text_area" v-if="!canBuy()">
+      <div class="background" />
+      <div class="lock_text">
+        <div class="text">
+          {{ lockText() }}
+        </div>
       </div>
     </div>
   </div>
@@ -62,69 +72,112 @@ export default {
     disabled(){
       return !this.canBuy();
     },
+    lockText(){
+      if(Savedata.hasShopItem(this.shopItem.id)){
+        return "購入済";
+      }
+      if(Savedata.coin() < this.shopItem.price){
+        return "お金が足りません";
+      }
+      return "";
+    }
   },
 }
 </script>
 
 <style lang="scss" scoped>
 @import "stylesheets/global_settings";
-.item{
-  display: flex;
-  border: 1px dotted $white;
-  &.disabled{
-    opacity: 0.5;
-  }
-  .icon_area{
-    width: 20%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    .icon{
-      width: 120px;
-      height: 80px;
+.wrapper{
+  width: 100%;
+  height: 100%;
+  position: relative;
+  .lock_text_area{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    .background{
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background-color: $bg3;
+      opacity: 0.3;
+    }
+    .lock_text{
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      .text{
+        background-color: $bg3;
+        line-height: 100%;
+        width: 50%;
+        text-align: center;
+      }
     }
   }
-  .main{
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
+  .item{
+    position: absolute;
+    width: 100%;
     height: 100%;
-    width: 45%;
-    padding: 8px;
-    .name{
+    display: flex;
+    border: 1px dotted $white;
+    &.disabled{
+      opacity: 0.5;
+    }
+    .icon_area{
+      width: 20%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      .icon{
+        width: 120px;
+        height: 80px;
+      }
+    }
+    .main{
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      height: 100%;
+      width: 45%;
+      padding: 8px;
+      .name{
       font-size: $font-size-large;
       line-height: 100%;
+      }
+      .description{
+        flex: 1;
+      }
     }
-    .description{
-      flex: 1;
+    .price_area{
+      width: 25%;
+      height: 100%;
+      display: flex;
+      gap: 8px;
+      align-items: center;
+      .coin_icon{
+        width: 48px;
+        height: 48px;
+      }
+      .price{
+        font-size: $font-size-large;
+        flex: 1;
+        text-align: center;
+      }
     }
-  }
-  .price_area{
-    width: 25%;
-    height: 100%;
-    display: flex;
-    gap: 8px;
-    align-items: center;
-    .coin_icon{
-      width: 48px;
-      height: 48px;
-    }
-    .price{
-      font-size: $font-size-large;
-      flex: 1;
-      text-align: center;
-    }
-  }
-  .buy_area{
-    width: 15%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    .buy_button{
-      width: 100px;
-      height: 64px;
-      padding: 8px;
+    .buy_area{
+      width: 15%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      .buy_button{
+        width: 100px;
+        height: 64px;
+        padding: 8px;
+      }
     }
   }
 }
