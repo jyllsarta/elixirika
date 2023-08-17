7437d72374d8e43d8d7f3a68721de75bbe204096
jyll sarta <illstarred.dive@gmail.com>
2023-08-17 20:06:27 +0900

    コンボ条件バグってた

--- assets/js/pirika_js/queens/packs/service/break_condition/combo_break_condition.js
+++ assets/js/pirika_js/queens/packs/service/break_condition/combo_break_condition.js
@@ -1,7 +1,7 @@
 export class ComboBreakCondition {
   getCountReduction(state, board, condition, card){
-    // コンボ数がカードの数値以上か(break判定時はboardにはまだcardが入っていないので+1する)
-    if(board.cards.length + 1 >= condition.param1){
+    // コンボ数がカードの数値と同じか(break判定時はboardにはまだcardが入っていないので+1する)
+    if(board.cards.length + 1 === condition.param1){
       console.log("combo matched");
       return 1;
     }
