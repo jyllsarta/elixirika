df1a026fe73ad8304002345100358255ada5913c
jyll sarta <illstarred.dive@gmail.com>
2023-09-18 00:10:49 +0900

    prettier消した

--- assets/.eslintrc.js
+++ assets/.eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-essential',
-    'prettier',
     'google',
   ],
   overrides: [
@@ -21,5 +20,8 @@ module.exports = {
     'max-len': ['error', {'code': 150}],
     'require-jsdoc': 0,
     'vue/multi-word-component-names': 0,
+    "quotes": [1, "double"],
+    "object-curly-spacing": ["error", "always"],
+    "indent": ["error", 2],
   },
 };
